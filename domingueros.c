c3f087a607613346f98e21a6693a4155650a939f
Barco55134 <60750782+Barco55134@users.noreply.github.com>
2020-04-15 13:51:12 +0200

    Update domingueros.c

--- domingueros.c
+++ domingueros.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+struct datos{
+	char nombre[30];
+	char apellidos[100];
+	int edad;
+	char email[100];
+	char lugar[30];
+};
+	
 int main () {
-	int a;
+	struct datos usuarios[100];
+	int opcion;
+	int cuentas=0;
 	
 	printf(" Bienvenido a DOMINGUEROS\n");
 	
@@ -15,16 +25,52 @@ int main () {
 	
 	printf("\n Seleccinar opcion tecleandola en el ordenador el número asociado a la opcion a la que se desea acceder.\n");
 	
-	scanf("%d", &a);
+	scanf("%d", &opcion);
 	
-	}while( a != 1 || a != 2 || a != 3 || a != 4 || a != 5 );
+	if ( opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5){
+		printf ("No se ha introducido un numero adecuado");
+	}
+
+	} while ( opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5 );
 	
-	switch(a) {
+	switch(opcion) {
 		case 1:
 			printf("Ha seleccionado la opcion de INICIAR SESION\n");
 			break;
 		case 2:
 			printf("Ha seleccionado la opcion de REGISTRARSE\n");
+			
+			printf ("Complete con sus datos\n");
+			
+			do {
+				
+			while(cuentas<=100){
+				printf ("Nombre:\t");
+				       gets (usuario[cuentas+1].nombre);
+					
+				printf ("Apellidos:\t");
+				       gets (usuario[cuentas+1].apellidos);
+					
+				printf ("Edad:\t");
+				       scanf ("%d", &usuario[cuentas+1].edad);
+					
+				printf ("Correo:\t");
+				       scanf ("%s", usuario[cuentas+1].email);
+				
+				printf ("Lugar de residencia:\t");
+				       gets (usuario[cuentas+1].lugar);
+			}	
+					
+			if (){
+				printf ("Debes completar con tus datos todos los campos");
+			} else {
+				printf("Cuenta creada con exito");
+				cuentas++;
+				printf("Ya somos %d domingueros!", &cuentas);
+			}
+					
+			} while ()
+					
 			break;
 		case 3:
 			printf("Ha seleccionado la opcion de NOVEDADES DE LA SEMANA\n");
@@ -36,7 +82,6 @@ int main () {
 			printf("Ha seleccionado la opcion de SALIR\n");
 			printf("Hasta pronto");
 			break;
-		default:
-			printf("Se ha introducido mal el numero\n");
+					
 	}
 }
