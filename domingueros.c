b8a34b4364544ada188d56df59118a47c1acd816
noeliagm <60703244+noeliagm@users.noreply.github.com>
2020-04-19 17:16:34 +0200

    Update domingueros.c

--- domingueros.c
+++ domingueros.c
@@ -27,18 +27,19 @@ int main () {
 		printf("\n (1)-Iniciar sesion \n");
 		printf("\n (2)-Registrarse \n");
 		printf("\n (3)-Novedades de la semana\n");
-		printf("\n (4)-Informacion sobre la aplicacion\n");
-		printf("\n (5)-Salir de la aplicacion\n");
+		printf("\n (4)-Todas las actividades\n");
+		printf("\n (5)-Informacion sobre la aplicacion\n");
+		printf("\n (6)-Salir de la aplicacion\n");
 	
 		printf("\n Seleccinar opcion tecleandola en el ordenador el número asociado a la opcion a la que se desea acceder.\n");
 	
 		scanf("%d", &opcion);
 	
-		if ( opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5){
+		if ( opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5 && opcion != 6){
 			printf ("No se ha introducido un numero adecuado");
 		}
 
-	} while ( opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5 );
+	} while ( opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5 && opcion != 6);
 	
 	switch (opcion) {
 		case 1:
@@ -202,9 +203,12 @@ int main () {
 			printf("Ha seleccionado la opcion de NOVEDADES DE LA SEMANA\n");
 			break;
 		case 4:
-			printf("Ha seleccionado la opcion de INFORMACION SOBRE LA APLICACION\n");
+			printf("Ha seleccionado la opcion de TODAS LAS ACTIVIDADES\n");
 			break;
 		case 5:
+			printf("Ha seleccionado la opcion de INFORMACION SOBRE LA APLICACION\n");
+			break;
+		case 6:
 			printf("Ha seleccionado la opcion de SALIR\n");
 			printf("Hasta pronto");
 			break;
