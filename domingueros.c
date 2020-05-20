66dcd379704be17b8a679ad4ca2f5ab3c372ef8c
Barco55134 <60750782+Barco55134@users.noreply.github.com>
2020-05-20 17:17:55 +0200

    Update domingueros.c

--- domingueros.c
+++ domingueros.c
@@ -3,6 +3,7 @@
 #include<stdbool.h> // Librería del tipo booleano (estándar)
 #include <stdlib.h> // Librería de rutinas de conversión estándar
 
+int lineasFichero();
 void resenyas();
 int Min_May(char palabra[50]);
 void recomendacion();
@@ -17,9 +18,8 @@ struct datos{
 	char nombre[30];
 	char apellidos[100];
 	int edad;
-	char apodo[20];
-	char contrasena[20];
 	char email[100];
+	char contrasena[20];
 	char lugar[30];
 };
 struct TActividad{
@@ -28,21 +28,23 @@ struct TActividad{
 };
 	
 int main () {
-	struct datos usuarios[100];
+	struct datos usuarios[100]; // Registrarse - Iniciar sesión
 	struct TActividad actividad[100];
-	int opcion, i, j;
-	int no_cuenta = 0, no_email = 0, ayuda;
-	bool punto = false, arroba = false, es_cuenta = false, diferente = false;
-	int cuentas = 0, longitud_ap = 0, longitud_con = 0;
-	char contrasena[20], apodo[20], email[100];
+	int opcion, j; //Registrarse
+	int no_cuenta = 0, no_email = 0, ayuda, i;
+	bool punto = false, arroba = false; // Registrarse
+	bool es_cuenta = false, diferente = false;
+	int cuentas = 0, longitud_con = 0; // Registrarse
+	char contrasena[20], email[100]; // Registrase
 	int nActividad=0;
 	int cont=1, num; // Novedades
 	char cadena[80]; // Novedades
 	srand (time(NULL)); //Novedades
 	FILE *pfichero;
 	FILE *nfichero; // Novedades
+	FILE *ufichero; // Registrarse - Iniciar sesion
 	
-	printf(" Bienvenido a DOMINGUEROS\n");
+	printf("\t********* Bienvenido a DOMINGUEROS *********\n");
 	
 	do{
 		printf("\n MENU \n");
@@ -54,7 +56,7 @@ int main () {
 		printf("\n (6)-Informacion sobre la aplicacion\n");
 		printf("\n (7)-Salir de la aplicacion\n");
 	
-		printf("\n Seleccinar opcion tecleandola en el ordenador el número asociado a la opcion a la que se desea acceder.\n");
+		printf("\n Seleccinar opcion tecleando el numero asociado a la opcion a la que se desea acceder.\n");
 	
 		scanf("%d", &opcion);
 	
@@ -136,99 +138,83 @@ int main () {
 			do {
 				printf ("Nombre:\t");
 				fflush (stdin);
-				gets (usuarios[cuentas].nombre);
-				
+				gets (usuarios[0].nombre);
 				printf ("\nApellidos:\t");
-				gets (usuarios[cuentas].apellidos);
+				gets (usuarios[0].apellidos);
 
-				if (usuarios[cuentas].nombre[0] == '\0' || usuarios[cuentas].apellidos[0] == '\0'){
+				if (usuarios[0].nombre[0] == '\0' || usuarios[0].apellidos[0] == '\0'){
 					printf ("ERROR. Debe completar adecuadamente con sus datos todos los campos\n");
 				}
 	
-			} while (usuarios[cuentas].nombre[0] == '\0' || usuarios[cuentas].apellidos[0] == '\0');
+			} while (usuarios[0].nombre[0] == '\0' || usuarios[0].apellidos[0] == '\0');
+			
+			strcat(usuarios[0].nombre, " ");
+			strcat(usuarios[0].nombre, usuarios[0].apellidos);
 			
 			printf ("\nEdad:\t");
-			scanf ("%d", &usuarios[cuentas].edad);
-			
-			if (usuarios[cuentas].edad <18){
-				printf("El contenido estara adaptado a su edad\n");
-			}
-			
-			do{
-				printf ("\nNombre de usuario o apodo:\t");
-				scanf ("%s", usuarios[cuentas].apodo);
-				
-				longitud_ap = strlen(usuarios[cuentas].apodo);
-	
-				if (usuarios[cuentas].apodo[0] == '\0'){ // ESTE CONDICIONAL NO ME LO LEE 
-					printf ("\nERROR. Debe completar adecuadamente con sus datos todos los campos\n");
-					
-				}
-				if (longitud_ap >8){
-					printf("Tu apodo es demasiado largo. Maximo 8 caracteres\n");
-					
-				} // else { // AQUI COMPROBAMOS SI EL APODO INTRODUCIDO COINCIDE CON ALGUNO YA REGISTRADO
-					//for (i=0; i<cuentas; i++){      	
-						//if (usuarios[i].apodo == usuarios[cuentas].apodo){
-							//diferente = false;
-							//printf ("Este nombre ya ha sido escogido. Intentelo de nuevo\n");
-						//} else{
-							//diferente = true;
-						//}
-					//} // HABRÁ QUE COMPROBARLO MÁS A DELANTE	
-				//} || diferente == false
-			} while ( usuarios[cuentas].apodo[0] == '\0' || longitud_ap >8);
-			
-			do{
-				printf ("\nContrasena:\n");
-				scanf ("%s", usuarios[cuentas].contrasena);
-				longitud_con = strlen(usuarios[cuentas].contrasena);
-				
-				if (usuarios[cuentas].contrasena[0] == '\0'){
-					printf ("\nERROR. Debe completar adecuadamente con sus datos todos los campos\n");
-					
-				}else if (longitud_con < 8 || usuarios[cuentas].contrasena[0] == '\0'){
-					printf ("Tu contrasena es demasiado corta. Minimo 8 caracteres\n");
-				}
-			} while (longitud_con < 8);
+			scanf ("%d", &usuarios[0].edad);
 			
 			do {
 				printf("\nCorreo:\t");
-				scanf("%s", usuarios[cuentas].email);
+				scanf("%s", usuarios[0].email);
 					
-				for (j=0; usuarios[cuentas].email[j]!= '\0';j++){
+				for (j=0; usuarios[0].email[j]!= '\0';j++){
 						
-					if (usuarios[cuentas].email[j] == '@'){
+					if (usuarios[0].email[j] == '@'){
 						arroba = true;
 					}
-					if (usuarios[cuentas].email[j] == '.'){
+					if (usuarios[0].email[j] == '.'){
 						punto = true;
 					}
 				}
 					
 				if (arroba == false || punto == false){
-					printf ("El correo introducido no es valido\n");
-					arroba = false;
-					punto = false;
+						printf ("El correo introducido no es valido\n");
+						arroba = false;
+						punto = false;
 				}
 					
 			} while (arroba == false || punto == false);
 			
+			do{
+				printf ("\nContrasena:\t");
+				scanf ("%s", usuarios[0].contrasena);
+				longitud_con = strlen(usuarios[0].contrasena);
 				
-			printf ("\nLugar de residencia en la Comunidad de Madrid:\t");
-			scanf ("%s", usuarios[cuentas].lugar);
-		
-			printf("Cuenta creada con exito");
-			printf("Ya somos %d domingueros!", cuentas+1);
-			cuentas++;	
+				if (usuarios[0].contrasena[0] == '\0'){ /////////////////////////////////////////////////////////NO SALE
+					printf ("\nERROR. Debe completar adecuadamente con sus datos todos los campos\n");
+				}
+				if (longitud_con < 8){
+					printf ("\nTu contrasena es demasiado corta. Minimo 8 caracteres\n");
+				}
+			} while (longitud_con < 8 || usuarios[0].contrasena[0] == '\0');
 			
+			printf ("\nLugar de residencia en la Comunidad de Madrid (o provincia):\t");
+			scanf ("%s", usuarios[0].lugar);	
+			
+			ufichero = fopen("USUARIOS.txt", "a+");
+			
+			if (ufichero == NULL) {
+				printf("No se encuentra el fichero\n");
+			}
+			
+			fprintf(ufichero, "%s - %d - %s - %s - %s\n", usuarios[0].nombre, usuarios[0].edad, usuarios[0].contrasena, usuarios[0].email, usuarios[0].lugar);
+			fclose(ufichero);
+			
+			cuentas = lineasFichero();
+			
+			printf("Cuenta creada con exito\n");
+			printf("Gracias %s por unirte a esta gran familia de viajeros\n", usuarios[0].nombre);
+			printf("Ya somos %d domingueros!\n", cuentas);	
+
 			break;
 		case 3:
 			printf("-----------------------NOVEDADES DE LA SEMANA-----------------------\n");
 			printf("\n");
 			printf("Hemos encontrado algunos CHOLLOS que puede que te interesen\n");
 			printf("Encontraras mas informacion de dichos eventos en las paginas oficiales correspondientes \n");
-	
+			printf("\n");
+			
 			nfichero = fopen ("NOVEDADES_DELA_SEMANA.txt", "r");
 	
 			if (nfichero == NULL) {
@@ -239,7 +225,7 @@ int main () {
 			num=rand()% 16+1; // Numero de linea aleatorio entre 1 y 16 = rand () % (N-M+1) + M
 			
 			// Recorremos el fichero línea a línea hasta el final del mismo, comparando si el contador de lineas, 'cont', es igual a un número aleatorio
-			// que obtenemos mediante la función rand(). Para ello incluímos la librería stdlib.h
+			// que obtenemos mediante la función rand(). [incluímos la librería stdlib.h]
 			while (((fgets(cadena, sizeof (cadena), nfichero))!= EOF)){ 
 				if(cont==num){
 					printf ("%s\n", cadena);
@@ -273,6 +259,7 @@ int main () {
 		case 5:
 			printf("-----------------------RESENYAS-----------------------\n");
 			printf("\n");
+			printf("Estos son los ultimos comentarios de algunos domingueros\n");
 			resenyas(); // Función que muestran por pantalla las reseñas que han sido introducidas anteriormente
 			recomendacion(); // Función que permite introducir reseñas y mostrarlas todas de nuevo
 			break;
@@ -281,10 +268,33 @@ int main () {
 			printf("\n");
 			break;
 		case 7:
-			printf("Ha seleccionado la opcion de SALIR\n");
-			printf("Hasta pronto");
+			adios();
 			break;		
 	}
+}
+void adios(){
+	printf("Ha seleccionado la opcion de salir\n");
+	printf("\n");
+	printf("\t******** Hasta pronto DOMINGUERO ********");
+}
+			       
+int lineasFichero(){
+	int cont=0;
+	int num_lineas = 0;
+	FILE *ufichero;
+	
+	if ((ufichero = fopen("USUARIOS.txt", "r")) == NULL){
+    	perror("USUARIOS.txt");
+      	printf("Error");
+   }	
+   while ((cont = fgetc(ufichero)) != EOF){
+   		if (cont == '\n'){
+   			num_lineas++;
+		}	
+   }
+   fclose(ufichero);
+   
+	return num_lineas;
 }
 
 void resenyas(){
@@ -313,6 +323,7 @@ void estrellas(int num, FILE*fichero){
 	fprintf(fichero,"PUNTUACION: %s\n", asteriscos);
 	fclose(fichero);
 }
+			       
 int Min_May(char palabra[50]){
 	int i=0;
 	for (i=0;palabra[i]!='\0';++i){
@@ -320,7 +331,8 @@ int Min_May(char palabra[50]){
 			palabra[i]=palabra[i]-32;
 		}
 	}
-}	
+}
+			       
 void recomendacion(){
 	int recomendar, i, muy_recomendado=4, esta_bien=3, mejorable=2, no_recomendado=1;
 	char nombre[50],apellidos[50],cadena[100];
@@ -328,7 +340,7 @@ void recomendacion(){
 	FILE*pfichero;
 	
 	printf("Para ESCRIBIR RECOMENDACION introduzca '1'. Despues debera escribir su nombre y apellidos\n");
-	printf("En caso contrario pulse 0\n");
+	printf("En caso contrario pulse 0 para  SALIR\n");
 	scanf("%d", &recomendar);
 	if(recomendar == 1){
 		printf("Nombre:\n");
@@ -381,6 +393,7 @@ void recomendacion(){
 		
 		resenyas();
 	} else if(recomendar == 0){
-		printf("Hasta luego");
+		adios();
 	}
 }
+			
